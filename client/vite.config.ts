5f94f8fbc9ecf3c1e9b2e9f13ffd82dbbeb9a06b
panupongdeve <panupong.dev.web@gmail.com>
2022-07-06 02:57:33 +0000

    [Fix] - mainifest

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      manifest: {
+        "name": "Seed Tracking",
+        "short_name": "Seed Tracking",
+        "start_url": "/",
+        "display": "standalone",
+        "theme_color": "#ffffff",
+        "icons": [
+            {
+                "src": "check-list-192.png",
+                "sizes": "192x192",
+                "type": "image/png"
+            },
+            {
+                "src": "check-list-144.png",
+                "sizes": "144x144",
+                "type": "image/png"
+            }
+    
+        ]
+      }
+    })
+  ]
 })
